71a9fe16141c9fb38c9b78544a21d4a0035a2133
eveban <everson_gsilva@yahoo.com.br>
2020-07-06 20:56:23 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,8 @@ import routes from './routes';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  }),
-);
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
