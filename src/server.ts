a289c4a39e614c95c729be6bc29456acffc65bec
eveban <everson_gsilva@yahoo.com.br>
2020-07-06 20:40:42 -0300

    Teste cors

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,15 @@ import routes from './routes';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    // methods: ['*'],
+    // responseHeader: ['Content-Type'],
+    // method: ['post'],
+    // maxAgeSeconds: 3600,
+  }),
+);
 app.use(express.json());
 
 app.use(routes);
