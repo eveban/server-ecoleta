1fb36ea657db15f4d1888b47a63f24b83c317df2
eveban <everson_gsilva@yahoo.com.br>
2020-07-06 22:13:23 -0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,16 @@ import path from 'path';
 import routes from './routes';
 
 const app = express();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
 
-app.use(
-  cors({
-    allowedHeaders: ['*'],
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
 
