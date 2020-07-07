9a086b3154f24f7cccbaa7fbaf9963d368c44100
eveban <everson_gsilva@yahoo.com.br>
2020-07-06 21:03:05 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,11 @@ import routes from './routes';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://ecoleta-agillitas.netlify.app/',
+  }),
+);
 
 app.use(routes);
 
