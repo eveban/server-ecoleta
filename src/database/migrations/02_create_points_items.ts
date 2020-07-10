722ff59ea821f8d34c63eb30781b5e9b8e8b0380
eveban <everson_gsilva@yahoo.com.br>
2020-07-10 16:33:23 -0300

    Corrigindo as Migrates

--- src/database/migrations/02_create_points_items.ts
+++ src/database/migrations/02_create_points_items.ts
@@ -4,14 +4,15 @@ export async function up(knex: Knex) {
   return knex.schema.createTable('points_items', table => {
     table.increments('id').primary();
     /** mysql */
-    table.integer('points_id').notNullable().unsigned();
+    // table.integer('points_id').notNullable().unsigned();
     /** sqlite3 */
-    // table.integer('points_id').notNullable();
+    table.integer('points_id').notNullable();
     table.foreign('points_id').references('id').inTable('points');
 
     /** sqlite3 */
-    // table.integer('items_id).notNullable();
-    table.integer('items_id').notNullable().unsigned();
+    table.integer('items_id').notNullable();
+    // mysql
+    // table.integer('items_id').notNullable().unsigned();
     table.foreign('items_id').references('id').inTable('items');
   });
 }
